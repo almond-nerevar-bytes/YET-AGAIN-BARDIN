975396659c9dbf5c09d29db8cfdc0ffd286c92ae
67 <Almond.Nerevar1999@gmail.com>
2026-09-24 17:24:40 -0300

    silly mistake

--- templates_data2/urls.py
+++ templates_data2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for templates_data project.
+URL configuration for templates_data2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
